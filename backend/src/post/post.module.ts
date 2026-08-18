2df663196faf83eb6ca68b924248e45441538c18
Siham Birhanu <sihambirhanu4@gmail.com>
2026-08-18 00:14:30 -0700

    feat: award progression points for approved posts

--- backend/src/post/post.module.ts
+++ backend/src/post/post.module.ts
@@ -1,11 +1,12 @@
 import { Module } from "@nestjs/common";
 import { PrismaModule } from "../prisma/prisma.module";
 import { ProgressModule } from "../progress/progress.module";
+import { VerificationModule } from "../verification/verification.module";
 import { PostController } from "./post.controller";
 import { PostService } from "./post.service";
 
 @Module({
-  imports: [PrismaModule, ProgressModule],
+  imports: [PrismaModule, ProgressModule, VerificationModule],
   controllers: [PostController],
   providers: [PostService],
   exports: [PostService],
